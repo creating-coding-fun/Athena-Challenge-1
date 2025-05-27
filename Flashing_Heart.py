3281a2d63b6abd7e8a006327c9d3e6539e8143b4
Space_Duck <marcnavya@gmail.com>
2025-05-27 14:51:19 -0400

    Hour 2 Commits
    What I changed,
    -moved screen and pen setup to the top
    -Changed it so the heart color fills pink and outline is red
    - attempted to make it flash pink and red

--- Flashing_Heart.py
+++ Flashing_Heart.py
@@ -1,13 +1,22 @@
 import turtle
 
+
+screen = turtle.Screen()
+screen.bgcolor('black')
+screen.setup(500, 450)
+
 pen = turtle.Turtle()
+pen.shape("turtle")
+turtle.pencolor('red')
+pen.speed(0)
+
 def curve():
     for i in range(200):
         turtle.right(1)
         turtle.forward(1)
 
-def heart(color):
-    turtle.color(color)
+def heart():
+    turtle.fillcolor('pink')
     turtle.begin_fill()
     turtle.left(140)
     turtle.forward(114)
@@ -16,31 +25,21 @@ def heart(color):
     curve()
     turtle.forward(115)
     turtle.end_fill()
+    turtle.done()
 
 def flash_heart(color1, color2):
-    heart(color1)
-    turtle.update()
-    turtle.ontimer(lambda:flash_heart(color2,color1), 100)
-    turtle.update()
     pen.clear()
+    pen.penup()
+    pen.position()
+    pen.setheading(0)
+    heart()
+    screen.update()
+    screen.ontimer(lambda: flash_heart(color1, color2), 500)
 
 
-
-screen = turtle.Screen()
-screen.bgcolor('black')
-screen.setup(500, 450)
-turtle.speed(0)
-turtle.pencolor('red')
 
 flash_heart('pink', 'red')
-
-screen.mainloop()
-
-
-
-
-
-
+heart()
 
 
 
