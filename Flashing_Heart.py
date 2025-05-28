9b0f448d5f31ec957da268a2b3028f7e5d16287a
Space_Duck <marcnavya@gmail.com>
2025-05-28 00:14:39 -0400

    Hour 4 Commits (Flashing_Heart.py, Last Commit for project1)
    What my final changes were:
    -Added a 4th color!! So one set of 4 colors flash, then another set of 4 colors, then another set of 4 colors
    
    -Added "I LOVE YOU!" text, each letter a different colors by estimating character length and doing some calculations
    
    -Tried to add sound (not in code), eventually gave up that idea after a bunch of things needing to be installed
    
    -Flashing Heart project done!!!!

--- Flashing_Heart.py
+++ Flashing_Heart.py
@@ -1,15 +1,35 @@
 import turtle
 
 
+
 screen = turtle.Screen()
 screen.bgcolor('black')
 screen.setup(500, 450)
 screen.tracer(0)
+screen.title("I LOVE YOU!!! <3")
 
 pen = turtle.Turtle()
 pen.shape("turtle")
 turtle.pencolor('red')
 pen.speed(0)
+
+text = turtle.Turtle()
+text.hideturtle()
+text.color("white")
+text.penup()
+text.goto(0, -100)
+
+colors = ["red", "yellow", "orange", "magenta", "cyan", "green", "purple", "pink", "blue"]
+msg = "  I LOVE YOU!"
+
+charc_width = 30
+start_point = -(len(msg) * charc_width)// 2
+
+for i, letter in enumerate(msg):
+    text.goto(start_point + i * charc_width, -100)
+    text.color(colors[i % len(colors)])
+    text.write(letter, align='center', font=('Arial', 26, 'bold'))
+
 
 def curve():
     for i in range(200):
@@ -27,35 +47,40 @@ def heart(color):
     pen.forward(115)
     pen.end_fill()
     screen.update()
-
-def flash_heart(color1, color2, color3,  current_color=1):
-    if current_color == 1:
-        heart(color1)
-        current_color = 2
-
-    elif current_color == 2:
-        heart(color2)
-        current_color = 3
-
-    else:
-        heart(color3)
-        current_color = 1
-
+def turtle_reset():
     pen.clear()
     pen.penup()
     pen.home()
     pen.pendown()
     pen.hideturtle()
     turtle.hideturtle()
-    screen.ontimer(lambda: flash_heart(color1, color2, color3,  current_color), 350)
+
+def flash_heart(color1, color2, color3, color4, current_color=1):
+    turtle_reset()
+
+    if current_color == 1:
+        heart(color1)
+        current_color = 2
+    elif current_color == 2:
+        heart(color2)
+        current_color = 3
+    elif current_color == 3:
+        heart(color3)
+        current_color = 4
+    else:
+        heart(color4)
+        current_color = 1
+
+    screen.ontimer(lambda: flash_heart(color1, color2, color3, color4,   current_color), 100)
+
 
 
 screen.update()
-flash_heart("pink", "red", "magenta")
-
-
-
+flash_heart("pink", "red", "magenta", "cyan")
+flash_heart("blue","gold", "purple", "white" )
+flash_heart("pink", "red", "magenta", "cyan")
 screen.mainloop()
+
 
 
 
